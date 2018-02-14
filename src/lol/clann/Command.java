b817d207c383e5aea3914a0b50e8f8751eac604d
= <azbh111@qq.com>
2018-02-14 21:33:32 +0800

    nothing

--- src/lol/clann/Command.java
+++ src/lol/clann/Command.java
@@ -5,7 +5,6 @@
  */
 package lol.clann;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import lol.clann.api.ItemApi;
 import lol.clann.api.PlayerApi;
@@ -24,17 +23,17 @@ import org.bukkit.inventory.PlayerInventory;
  */
 @AutoRegister
 public class Command extends CustomCommand {
-    
+
     public Command() {
         super(Clann.plugin, "base");
     }
-    
+
     @SubCommandAnnotation(mustPlayer = false, args = "(player)", des = "查看玩家afk状态")
     public void afk(CommandSender sender, String[] args) {
         sender.sendMessage("AFK:" + Clann.plugin.afkdata.data.get(args[0]).isAFK());
         sender.sendMessage("状态:" + Clann.plugin.afkdata.data.get(args[0]).actions.toString());
     }
-    
+
     @SubCommandAnnotation(mustPlayer = true, args = "(key)", des = "用key保存背包物品")
     public void saveInv(Player player, String[] args) {
         ArrayList<ItemStack> arr = new ArrayList();
@@ -49,14 +48,10 @@ public class Command extends CustomCommand {
             player.sendMessage("您背包为空");
             return;
         }
-        try {
-            ItemApi.saveItemStacks(args[0], arr.toArray(new ItemStack[arr.size()]));
-        } catch (IOException ex) {
-            throw new CEException("保存异常", ex);
-        }
+        ItemApi.saveItemStacks(args[0], arr.toArray(new ItemStack[arr.size()]));
         player.sendMessage("保存成功,key=" + args[0]);
     }
-    
+
     @SubCommandAnnotation(mustPlayer = true, args = "(key)", des = "读取key对应的物品")
     public void getInv(Player player, String[] args) {
         ItemStack[] iss;
